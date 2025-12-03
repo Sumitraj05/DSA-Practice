206d9363ede75741be028f88c12789a0d07d1e3d
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2025-12-03 15:09:34 +0530

    Implement trapping rain water solution

---
+++ Stack/LeetCode- Code Testcase Testcase Test Result 42. Trapping Rain Water/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int res = 0;
+        int left = 0;
+        int right = n-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        for(int i = 0; i<n; i++){
+            if(height[left]<=height[right]){
+                if(height[left]>=leftMax){
+                    leftMax = height[left];
+                }else{
+                    res += leftMax-height[left];
+                }
+                left++;
+            }else{
+                if(height[right]>=rightMax){
+                    rightMax = height[right];
+                }else{
+                    res += rightMax-height[right];
+                }
+                right--;
+            }
+        }
+        return res;
+    }
+}
