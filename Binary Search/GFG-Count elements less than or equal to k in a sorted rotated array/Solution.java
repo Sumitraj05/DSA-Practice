708fc0e8d34b3195702a66eb71699046d5e41f90
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2025-12-24 13:08:38 +0530

    Optimize countLessEqual method for sorted rotated array
    Refactored countLessEqual method to avoid sorting and implemented binary search for efficiency.

--- Binary Search/GFG-Count elements less than or equal to k in a sorted rotated array/Solution.java
+++ Binary Search/GFG-Count elements less than or equal to k in a sorted rotated array/Solution.java
@@ -1,17 +1,34 @@
 class Solution {
-    public int countLessEqual(int[] arr, int x) {
-        // code here
-        int i = 0;
-        int j = arr.length-1;
-        Arrays.sort(arr);
+    public int binarySearch(int[] arr, int i, int j, int x){
+        int temp = i;
         while(i<=j){
-            int mid = (i+j)/2;
+            int mid = i + (j-i)/2;
             if(arr[mid] <= x){
                 i = mid+1;
             }else{
                 j = mid-1;
             }
         }
-        return i;
+        return i-temp;
+    }
+    public int countLessEqual(int[] arr, int x) {
+        // code here
+        int i = 0;
+        int j = arr.length-1;
+        int rot = 0;
+        while(i<j){
+            int mid = i + (j-i)/2;
+            if(arr[mid]>arr[j]){
+                i = mid+1;
+            }else{
+                j = mid;
+            }
+        }
+        rot = j;
+        
+        int count = binarySearch(arr, 0, rot-1, x);
+        count += binarySearch(arr, rot, arr.length-1, x);
+        return count;
+        
     }
 }
