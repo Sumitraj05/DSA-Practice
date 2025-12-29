5ee046fed8464b2c0d558410ae306a76152af8f4
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2025-12-29 11:51:25 +0530

    Create Solution.java

---
+++ Binary Search/GFG-K-th element of two Arrays/Solution.java
@@ -0,0 +1,79 @@
+
+
+// User function Template for Java
+
+class Solution {
+    public int kthElement(int a[], int b[], int k) {
+        // code here
+        if(a.length > b.length) return kthElement(b, a, k);
+        int n = a.length;
+        int m = b.length;
+        int low = Math.max(0, k-m);
+        int high = Math.min(n, k);
+        while(low<=high){
+            int mid1 = (low+high) >> 1;
+            int mid2 = k-mid1;
+            int l1 = mid1 == 0 ? Integer.MIN_VALUE:a[mid1-1];
+            int l2 = mid2 == 0 ? Integer.MIN_VALUE:b[mid2-1];
+            int r1 = mid1 == n ? Integer.MAX_VALUE:a[mid1];
+            int r2 = mid2 == m ? Integer.MAX_VALUE:b[mid2];
+            
+            if(l1<=r2 && l2 <= r1){
+                return Math.max(l1, l2);
+            }else if(l1 > r2){
+                high = mid1-1;
+            }else{
+                low = mid1+1;
+            }
+        }
+        return 1;
+    }
+}
+
+
+
+// import java.util.*;
+
+// public class tUf {
+//     public static int kthElement(ArrayList<Integer> a, ArrayList<Integer> b, int m, int n, int k) {
+//         if (m > n) return kthElement(b, a, n, m, k);
+
+//         int left = k; // length of left half
+
+//         // apply binary search:
+//         int low = Math.max(0, k - n), high = Math.min(k, m);
+//         while (low <= high) {
+//             int mid1 = (low + high) >> 1;
+//             int mid2 = left - mid1;
+//             // calculate l1, l2, r1, and r2
+//             int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+//             int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+//             if (mid1 < m) r1 = a.get(mid1);
+//             if (mid2 < n) r2 = b.get(mid2);
+//             if (mid1 - 1 >= 0) l1 = a.get(mid1 - 1);
+//             if (mid2 - 1 >= 0) l2 = b.get(mid2 - 1);
+
+//             if (l1 <= r2 && l2 <= r1) {
+//                 return Math.max(l1, l2);
+//             }
+
+//             // eliminate the halves:
+//             else if (l1 > r2) high = mid1 - 1;
+//             else low = mid1 + 1;
+//         }
+//         return 0; // dummy statement
+//     }
+
+//     public static void main(String[] args) {
+//         ArrayList<Integer> a = new ArrayList<>();
+//         a.add(2); a.add(3); a.add(6); a.add(7); a.add(9);
+
+//         ArrayList<Integer> b = new ArrayList<>();
+//         b.add(1); b.add(4); b.add(8); b.add(10);
+
+//         System.out.println("The k-th element of two sorted arrays is: " +
+//                             kthElement(a, b, a.size(), b.size(), 5));
+//     }
+// }
+        
+        
