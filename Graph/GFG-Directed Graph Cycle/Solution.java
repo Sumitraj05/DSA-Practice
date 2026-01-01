fe7bd2661084f30be7cdc19de6da3f0e7bad8964
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-01-01 22:55:50 +0530

    Update Solution.java

--- Graph/GFG-Directed Graph Cycle/Solution.java
+++ Graph/GFG-Directed Graph Cycle/Solution.java
@@ -1,3 +1,4 @@
+// Using DFS Method
 class Solution {
     public boolean dfs(ArrayList<ArrayList<Integer>> adj, int[] vis, int[] path, int idx){
         vis[idx] = 1;
@@ -34,5 +35,45 @@ class Solution {
             }
         }
         return false;
+    }
+}
+
+// Using Kanh's Algorithm - Topological Sort
+class Solution {
+    
+    public boolean isCyclic(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i<V; i++){
+            adj.add(new ArrayList<>());
+        }
+        int n = edges.length;
+        for(int i = 0 ; i<n; i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        
+        int[] indegree = new int[V];
+        for(int i = 0; i<V; i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        for(int i = 0; i<V; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            cnt++;
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.add(it);
+                }
+            }
+        }
+        return cnt != V;
     }
 }
