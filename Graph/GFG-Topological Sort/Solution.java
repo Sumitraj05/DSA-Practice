c7d8ad9832781abaedd29be5531047e7721921ac
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-01-01 22:44:39 +0530

    Update Solution.java

--- Graph/GFG-Topological Sort/Solution.java
+++ Graph/GFG-Topological Sort/Solution.java
@@ -1,3 +1,4 @@
+// Using DFS Method
 class Solution {
     
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
@@ -34,5 +35,47 @@ class Solution {
             }
         }
         stack.push(node);
+    }
+}
+
+// Using Kahn's Algorithm - BFS
+class Solution {
+    
+    public ArrayList<Integer> topoSort(int V, int[][] edges) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i<V; i++){
+            adj.add(new ArrayList<>());
+        }
+        int n = edges.length;
+        for(int i = 0 ; i<n; i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        
+        int[] indegree = new int[V];
+        for(int i = 0; i<V; i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        for(int i = 0; i<V; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int node = q.poll();
+            ans.add(node);
+            
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.add(it);
+                }
+            }
+        }
+        return ans;
     }
 }
