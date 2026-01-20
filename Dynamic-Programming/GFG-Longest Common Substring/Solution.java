44813559609969bed43733592cd1702a8d1963f3
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-01-20 17:16:24 +0530

    Create Solution.java

---
+++ Dynamic-Programming/GFG-Longest Common Substring/Solution.java
@@ -0,0 +1,45 @@
+
+// Bottom Up -> Memoization will not work here make it more complex
+class Solution {
+    public int longestCommonSubstr(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    ans = Math.max(ans, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+// Space Optimization
+class Solution {
+    public int longestCommonSubstr(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[] prev = new int[m+1];
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[m+1];
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    curr[j] = 1 + prev[j - 1];
+                    ans = Math.max(ans, curr[j]);
+                } else {
+                    curr[j] = 0;
+                }
+            }
+            prev = curr;
+        }
+        return ans;
+    }
+}
