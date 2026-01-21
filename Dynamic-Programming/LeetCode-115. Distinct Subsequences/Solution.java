239fb6cde591a4df3f6ed6d8df3006bda5582a2d
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-01-21 12:04:34 +0530

    Create Solution.java

---
+++ Dynamic-Programming/LeetCode-115. Distinct Subsequences/Solution.java
@@ -0,0 +1,71 @@
+// Memoization
+class Solution {
+    public int solve(int[][] dp, String s, String t, int i, int j){
+        if(j<0){
+            return 1;
+        }
+        if(i<0){
+            return 0;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s.charAt(i) == t.charAt(j)){
+            return dp[i][j] = solve(dp, s, t, i-1, j-1) + solve(dp, s, t, i-1, j);
+        }else{
+            return dp[i][j] = solve(dp, s, t, i-1, j);
+        }
+    }
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n][m];
+        for(int[] ar : dp){
+            Arrays.fill(ar, -1);
+        }
+        return solve(dp, s, t, n-1, m-1);
+    }
+}
+
+// Bottom Up
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
+
+// Space Optimization
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[] prev = new int[m+1];
+        prev[0] = 1;
+        for(int i = 1; i<=n; i++){
+            int[] curr = new int[m+1];
+            curr[0] = 1;
+            for(int j = 1; j<=m; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    curr[j] = prev[j-1] + prev[j];
+                }else{
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+}
