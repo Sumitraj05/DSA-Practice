2b23a472a65e9faee602be678d335fdd44638616
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-01-22 02:50:48 +0530

    Create Solution.java

---
+++ Dynamic-Programming/LeetCode-673. Number of Longest Increasing Subsequence/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findNumberOfLIS(int[] arr) {
+        // Code here
+        int n = arr.length;
+        int[] dp = new int[n];
+        int [] cnt = new int[n];
+        Arrays.fill(dp, 1);
+        Arrays.fill(cnt, 1);
+        int maxLen = 1;
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<i; j++){
+                if(arr[i]>arr[j] && dp[j] + 1 > dp[i]){
+                    dp[i] = dp[j]+1;
+                    cnt[i] = cnt[j];
+                }
+                else if(arr[i]>arr[j] && dp[j]+1 == dp[i]){
+                    cnt[i] += cnt[j];
+                }
+            }
+            if(dp[i] > maxLen){
+                maxLen = dp[i];
+            }
+        }
+        int maxi = 0;
+        for(int i = 0; i<n; i++){
+            if(dp[i] == maxLen){
+                maxi += cnt[i];
+            }
+        }
+        return maxi;
+    }
+}
