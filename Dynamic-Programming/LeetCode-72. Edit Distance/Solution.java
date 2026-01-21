8f2e2dcfef57a4bbb0b471b6e779a177863a4db9
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-01-21 12:03:52 +0530

    Create Solution.java

---
+++ Dynamic-Programming/LeetCode-72. Edit Distance/Solution.java
@@ -0,0 +1,78 @@
+// Memoization
+class Solution {
+    public int solve(int[][] dp, String s, String t, int i, int j){
+        if(i<0){
+            return j+1;
+        }
+        if(j<0){
+            return i+1;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s.charAt(i) == t.charAt(j)){
+            return dp[i][j] = solve(dp, s, t, i-1, j-1);
+        }
+        int insert = 1 + solve(dp, s, t, i, j-1);
+        int delete = 1 + solve(dp, s, t, i-1, j);
+        int replace = 1 + solve(dp, s, t, i-1, j-1);
+        return dp[i][j] = Math.min(insert, Math.min(delete, replace));
+    }
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(); 
+        int m = word2.length();
+        int[][] dp = new int[n][m];
+        for(int[] ar : dp){
+            Arrays.fill(ar, -1);
+        }
+        return solve(dp, word1, word2, n-1, m-1);
+    }
+}
+
+// Bottom Up
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(); 
+        int m = word2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int j = 0; j<=m; j++){
+            dp[0][j] = j;
+        }
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = i;
+        }
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
+
+// Space Optimization
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(); 
+        int m = word2.length();
+        int[] prev = new int[m+1];
+        for(int j = 0; j<=m; j++){
+            prev[j] = j;
+        }
+        for(int i = 1; i<=n; i++){
+            int[] curr = new int[m+1];
+            curr[0] = i;
+            for(int j = 1; j<=m; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    curr[j] = prev[j-1];
+                }else{
+                    curr[j] = 1 + Math.min(prev[j], Math.min(prev[j-1], curr[j-1]));
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+}
