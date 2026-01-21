6b0b4bc4d4d6e99435d6e36e75d264e68332f6fe
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-01-22 01:26:55 +0530

    Create Solution.java

---
+++ Dynamic-Programming/GFG-Print Longest Increasing Subsequence/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public ArrayList<Integer> getLIS(int arr[]) {
+        // Code here
+        int n = arr.length;
+        int[] dp = new int[n];
+        int [] prev = new int[n];
+        Arrays.fill(dp, 1);
+        for(int i = 0; i<n; i++){
+            prev[i] = i;
+        }
+        int maxInd = 0;
+        int maxLen = 1;
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<i; j++){
+                if(arr[i]>arr[j] && dp[j] + 1 > dp[i]){
+                    dp[i] = dp[j]+1;
+                    prev[i] = j;
+                }
+            }
+            if(dp[i] > maxLen){
+                maxLen = dp[i];
+                maxInd = i;
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(prev[maxInd] != maxInd){
+            ans.add(arr[maxInd]);
+            maxInd = prev[maxInd];
+        }
+        ans.add(arr[maxInd]);
+        Collections.reverse(ans);
+        return ans;
+    }
+}
