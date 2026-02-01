ce57aaaf6bdad3762d82a09349d89347aacf0b87
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-02-01 10:51:35 +0530

    Create Solution.java

---
+++ Simulation/LeetCode-3010. Divide an Array Into Subarrays With Minimum Cost I/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumCost(int[] nums) {
+        int first = nums[0];
+        int n = nums.length;
+        int second = Integer.MAX_VALUE;
+        int third = Integer.MAX_VALUE;
+        for(int i = 1; i<n; i++){
+            if(nums[i] < second){
+                third = second;
+                second = nums[i];
+            }
+            else if(nums[i]< third){
+                third = nums[i];
+            }
+        } 
+        return first+second+third;
+    }
+}
