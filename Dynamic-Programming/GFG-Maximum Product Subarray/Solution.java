9c7743396024e7e01069226c6dd31cb56c8dcfd0
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-02-08 11:57:11 +0530

    Refactor maxProduct to use prefix and suffix products

--- Dynamic-Programming/GFG-Maximum Product Subarray/Solution.java
+++ Dynamic-Programming/GFG-Maximum Product Subarray/Solution.java
@@ -19,18 +19,15 @@ class Solution {
     int maxProduct(int[] arr) {
         // code here
         int n = arr.length;
-        int max = arr[0];
-        int min = arr[0];
-        int ans = arr[0];
-        for(int i = 1; i<n; i++){
-            if(arr[i]<0){
-                int temp = max;
-                max = min;
-                min = temp;
-            }
-            max = Math.max(max*arr[i], arr[i]);
-            min = Math.min(min*arr[i], arr[i]);
-            ans = Math.max(ans, max);
+        int pref = 1;
+        int suff = 1;
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0; i<n; i++){
+            if(pref == 0) pref = 1;
+            if(suff == 0) suff = 1;
+            pref *= arr[i];
+            suff *= arr[n-1-i];
+            ans = Math.max(ans, Math.max(pref, suff));
         }
         return ans;
     }
