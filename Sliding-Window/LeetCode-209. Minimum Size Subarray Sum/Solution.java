54bc5d9260a745f861716d88ff84f715e65f807c
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-02-08 15:27:36 +0530

    Add solution for Minimum Size Subarray Sum problem
    Implement the minSubArrayLen method to find the minimum size of a subarray that meets or exceeds the target sum.

---
+++ Sliding-Window/LeetCode-209. Minimum Size Subarray Sum/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+       int n = nums.length;
+       int left = 0;
+       int res = Integer.MAX_VALUE;
+       int sum = 0;
+       for(int right = 0; right<n; right++){
+            sum += nums[right];
+            while(sum >= target){
+                res = Math.min(res, right-left+1);
+                sum -= nums[left];
+                left++;
+            }
+       }
+       return res == Integer.MAX_VALUE ? 0 : res;
+    }
+}
