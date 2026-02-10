720f3225e317848be97506646fc966f081cbdc07
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2026-02-11 00:35:23 +0530

    Create Solution.java

---
+++ Linked-List/LeetCode-83. Remove Duplicates from Sorted List/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = head;
+        while(curr != null){
+            while(curr.next != null && curr.val == curr.next.val){
+                curr = curr.next;
+            }
+            prev.next = curr.next;
+            prev = prev.next;
+            curr = curr.next;
+        }
+        return head;
+    }
+}
